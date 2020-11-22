9c1e403c2e707538f5d77f7b587e20f88357992c
meidix <mahdihossieni@gmail.com>
2020-11-22 04:35:43 +0330

    files added

---
+++ baraction.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# baraction.sh for spectrwm status bar
+
+## DISK
+hdd() {
+  hdd="$(df -h | awk 'NR==4{print $3, $5}')"
+  echo -e "HDD: $hdd"
+}
+
+## RAM
+mem() {
+  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "Mem: $mem"
+}
+
+## CPU
+cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "CPU: $cpu%"
+}
+
+## Language Layout
+lang_layout() {
+	layout="$(setxkbmap -query | grep layout)"
+	echo -e "$layout"
+}
+
+
+
+SLEEP_SEC=3
+#loops forever outputting a line every SLEEP_SEC secs
+
+# It seems that we are limited to how many characters can be displayed via
+# the baraction script output. And the the markup tags count in that limit.
+# So I would love to add more functions to this script but it makes the 
+# echo output too long to display correctly.
+while :; do
+	echo "+@fg=1; +@fn=1;💻+@fn=0; $(cpu) +@fg=0; | +@fg=2; +@fn=1;💾+@fn=0; $(mem) +@fg=0; | +@fg=3; +@fn=1;💿+@fn=0; $(hdd) +@fg=0; | +@fg=4; +@fn=1;🔈+@fn=0;$(lang_layout)   | "
+	sleep $SLEEP_SEC
+done
